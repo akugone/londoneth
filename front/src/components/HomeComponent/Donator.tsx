78d890af95ca3438921de580a51c94b9929c1a86
akugone <dev@akumaw.com>
2024-03-16 11:00:57 +0000

    Update Donator component styling

--- front/src/components/HomeComponent/Donator.tsx
+++ front/src/components/HomeComponent/Donator.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 
 function Donator() {
     return (
-        <div className="flex flex-col items-center gap-2 text-center">
+        <div className="ml-5 flex flex-col items-center gap-2 text-center">
             <Image
                 src="/images/nouns/heart.png"
                 width={100}
@@ -16,7 +16,7 @@ function Donator() {
                 Are you an Donator?
             </h1>
             <p className="text-sm text-muted-foreground">
-                Create your first event to collect donation to charities
+                Make your first donation to a charity
             </p>
             <Link
                 className="block my-4 bg-primary text-primary-foreground shadow hover:bg-primary/90 rounded-md px-10  py-3 text-md"
